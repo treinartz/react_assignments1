29cf51c2b5a7c2949e48b320905971aa6476e8ad
Tom Reinartz <tomreinartz@Toms-MacBook-Pro.local>
2025-12-26 06:30:18 -0600

    ex13

--- week13/exercise1/src/App.jsx
+++ week13/exercise1/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Hello World! This is week11 exercise1</h1>
+      <h1>Hello World this is week13 exercise1!</h1>
     </div>
   );
 }
