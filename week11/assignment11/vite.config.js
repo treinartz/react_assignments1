57d2e040a698678b05510005e6f7765365b79ce0
Tom Reinartz <tomreinartz@Toms-MacBook-Pro.local>
2025-12-27 08:45:12 -0600

    week11 done

---
+++ week11/assignment11/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base:'/react_assignment1/week11/assignment11/',
+  plugins: [react()],
+})
